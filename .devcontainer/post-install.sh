16a3499e46c3c9fa773fcd56c29f8effd4356ebe
Jan Wiegelmann <jan@Jans-Mac-mini.local>
2026-04-20 18:49:48 +0200

    update install sh

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -x
 
+export HOME="/root"
+export NVM_DIR="$HOME/.nvm"
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 24
+nvm use 24
+
 npm install typescript --save-dev
 
 
